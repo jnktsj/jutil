2ca04c7514be0df5dbed5ca644140049d29ca882
Junko Tsuji <jtsuji@broadinstitute.org>
2022-03-09 10:47:40 -0500

    minor tweak

--- vm_setup/1_base-config.sh
+++ vm_setup/1_base-config.sh
@@ -53,7 +53,7 @@ export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install gcsfuse
+sudo apt-get install -y gcsfuse
 
 
 # clean VM at the end
