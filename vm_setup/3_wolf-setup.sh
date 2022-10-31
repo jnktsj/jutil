e627f81e6fb63877625d806c0191218cee1402a5
Junko Tsuji <jtsuji@broadinstitute.org>
2022-10-31 16:07:25 -0400

    updated to the latest syntax

--- vm_setup/3_wolf-setup.sh
+++ vm_setup/3_wolf-setup.sh
@@ -4,21 +4,27 @@ sudo mkdir /mnt/nfs && sudo chmod 777 /mnt/nfs
 # Install required packages for wolF
 sudo apt-get install -y nfs-common docker.io nfs-kernel-server
 sudo pip3 install docker-compose google-crc32c
-sudo pip3 install prefect==0.14.16
+#sudo pip3 install prefect==0.14.16  # might not be needed anymore
 
 # Change docker permission to run it without sudo
 sudo groupadd docker; sudo usermod -aG docker $USER
+newgrp docker
+
 sudo chmod 666 /var/run/docker.sock
 
 # Download Getz lab tools
 cd ~/ && mkdir src && cd src
 gsutil cp gs://getzlab-secrets/github-service-account/github ~/.ssh/getzlab && chmod 400 ~/.ssh/getzlab
+
 GIT_SSH_COMMAND='ssh -i ~/.ssh/getzlab -o IdentitiesOnly=yes' git clone git@github.com:getzlab/canine.git
 cd canine && sudo pip3 install . && cd ../
+
 GIT_SSH_COMMAND='ssh -i ~/.ssh/getzlab -o IdentitiesOnly=yes' git clone git@github.com:getzlab/wolF.git
 cd wolF && sudo pip3 install . && cd ../
+
 GIT_SSH_COMMAND='ssh -i ~/.ssh/getzlab -o IdentitiesOnly=yes' git clone git@github.com:getzlab/slurm_gcp_docker.git
-cd slurm_gcp_docker
+cd slurm_gcp_docker/src
+./setup.sh
 
 # Run this manually
 #  1. Add new nodes (e.g. n1-highmem-16 , etc) to 'nodetypes.json' in './conf'
